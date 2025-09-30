aabb3a9cc5858164979838768a67125161d7b126
UDADDY <gustmd5715@gmail.com>
2025-09-30 20:17:02 +0900

    프로젝트 구성

---
+++ src/main/kotlin/cs/capstone/CapstoneApplication.kt
@@ -0,0 +1,11 @@
+package cs.capstone
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CapstoneApplication
+
+fun main(args: Array<String>) {
+	runApplication<CapstoneApplication>(*args)
+}
